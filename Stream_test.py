8b2cd664af0d5e43fcda13dc08bedc44c335a682
NGK87 <52498962+NGK87@users.noreply.github.com>
2019-09-23 09:30:15 +0200

    Use of stream API instead of Search API
    Tracking of French Insurance Companies
    ==> Next step : Add the emotional states to the program

---
+++ Stream_test.py
@@ -0,0 +1,43 @@
+import tweepy
+
+# my user credential in order to access TWITTER API
+consumer_key = "mFBcbp8mZkZlEVAFcarY0JQEv"
+consumer_secret = "hhuzxiV1EZQOc4sY9e5qhzFOxHauC8s5ydaxQER2NLqwwhtcGm"
+access_token = "624059394-D881rOiRH0xTpkrP2SxOZor09jOiV6m1NFtYSMZM"
+access_token_secret = "moT59pM1dNi0GxRuUNXR9O1tddU0Wd2uCTYT4T9wYtvu3"
+
+# Authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create API Object
+api = tweepy.API(auth)
+
+
+# Print own timeline
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#  print(tweet.text)
+
+# Override method in StreamListener
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.user.screen_name, status.text, status.created_at, status.retweet_count)
+
+
+# Creating a Stream
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+# Starting A Stream
+myStream.filter(track=['CNP Assurances, Axa, Credit Agricole, Generali, BNP Paribas, Groupama, Covéa, Covea, \
+                       Credit Mutuel, Crédit Mutuel, Allianz, Société Générale, AG2R, Aviva, Macif, Natixis, \
+                       Swiss Life, AIG, MAIF, SMABTP, HSBC, MACSF, A Comme Assure, Active Assurances, ADAP, Affineo, \
+                       AGPM, AllSecure, Alptis, Amaguiz, AMV, Aon Assurances, April, Areas Assurances, Assu2000, \
+                       Assurance Outremer, Assureo, Assuréo, AssurOnlinem, Assurpeople, Axeria, Camacte, Car-y, \
+                       Carrefour Assurances, CGPA, Direct Assurances, Elsassur, Euro Assurances, Generali France, \
+                       Groupama, HEYME, Hiscox, Homebrella, Interassurances, La Banque Postale, La Medicale, La Rurale,\
+                       Le Finistere Assurance, Le Finistère Assurance, Lovys, Luko, MACSF, MAE, MALJ, Matmut, MAPA, \
+                       Monceau Assurances, Mutuaide, Mutuelle de Poitiers Assurances, Opteven, Otherwise, Protech BTP, \
+                       SelfAssurance, SMA SA, Sogessur, Thélem Assurances, Verspieren, Zenith Assurance'], \
+                languages=['fr']) # the comma will cause the programto search any of those terms. for searching all the terms, use spaces only
